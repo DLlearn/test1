001f346c7676208870c428c637ae1e6ff8439df2
田亮 <tianlianglll@foxmail.com>
2020-03-06 10:03:31 +0800

    adf

---
+++ a.py
@@ -0,0 +1,23 @@
+import numpy as np
+import tensorflow as tf
+
+a = 4
+def f(x):
+    x=x+3
+    return x
+
+b =6
+a = 4
+b =6
+a = 4
+b =6
+a = 4
+b =6
+f(6)
+a = 4
+b =6
+a = 4
+b =6
+a = 4
+b =6
+print(tf.__version__)
